b5625ed0395ac5d67ebab7ced4f21dfc3e293e5f
George Nikolaidis <nove@novesoftware.net>
2025-05-13 19:51:30 +0300

    Add Serilog configuration in appsettings.json for proper log output

--- ChronoQuest.WebApi/Program.cs
+++ ChronoQuest.WebApi/Program.cs
@@ -8,7 +8,7 @@ Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
     .Enrich.FromLogContext()
     .WriteTo.Console()
-    .CreateLogger();
+    .CreateBootstrapLogger();
 
 try
 {
