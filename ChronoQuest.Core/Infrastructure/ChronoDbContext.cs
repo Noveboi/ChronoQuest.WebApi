8a4e4f36d46be7ee6b031dd3a66c7afa8a0f2f42
Nove <cptgiorgos03@gmail.com>
2025-05-13 17:03:59 +0300

    Remove

--- ChronoQuest.Core/Infrastructure/ChronoDbContext.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore;
-
-namespace ChronoQuest.Core.Infrastructure;
-
-internal sealed class ChronoDbContext(DbContextOptions<ChronoDbContext> options) : DbContext(options)
-{
-    
-}
