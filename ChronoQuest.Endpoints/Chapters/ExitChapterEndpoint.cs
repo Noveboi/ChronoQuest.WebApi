754dd6387a932fd8257136085eaf0d15abb825a4
Nove <cptgiorgos03@gmail.com>
2025-05-15 10:40:59 +0300

    Add endpoints for starting and stopping the reading of a chapter

---
+++ ChronoQuest.Endpoints/Chapters/ExitChapterEndpoint.cs
@@ -0,0 +1,25 @@
+using ChronoQuest.Core.Application;
+using FastEndpoints;
+
+namespace ChronoQuest.Endpoints.Chapters;
+
+internal sealed class ExitChapterEndpoint(IUserActionTracker<ChapterTrackingInformation> tracker) : Endpoint<GetChapterRequest>
+{
+    public override void Configure()
+    {
+        Get("{chapterId:guid}/stop-reading");
+        Group<ChapterGroup>();
+    }
+
+    public override async Task HandleAsync(GetChapterRequest req, CancellationToken ct)
+    {
+        var info = await tracker.StopTrackingAsync(userId: req.UserId, entityId: req.ChapterId, ct);
+        if (info is null) 
+        {
+            await SendNoContentAsync(ct);
+            return;
+        }
+
+        await SendOkAsync(ct);
+    }
+}
---
+++ ChronoQuest.Endpoints/Chapters/GetChapterEndpoint.cs
@@ -0,0 +1,19 @@
+using ChronoQuest.Core.Application;
+using FastEndpoints;
+
+namespace ChronoQuest.Endpoints.Chapters;
+
+internal sealed class ReadChapterEndpoint(IUserActionTracker<ChapterTrackingInformation> tracker) : Endpoint<GetChapterRequest>
+{
+    public override void Configure()
+    {
+        Get("{chapterId:guid}");
+        Group<ChapterGroup>();
+    }
+
+    public override async Task HandleAsync(GetChapterRequest req, CancellationToken ct)
+    {
+        await tracker.StartTrackingAsync(userId: req.UserId, entityId: req.ChapterId, ct);
+        await SendOkAsync(ct);
+    }
+}
