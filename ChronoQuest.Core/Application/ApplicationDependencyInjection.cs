da612ddd8d71bf066cc5d89c6e8ce60dc09d3751
Nove <cptgiorgos03@gmail.com>
2025-05-15 10:40:06 +0300

    Add Core.Application DI

---
+++ ChronoQuest.Core/Application/ApplicationDependencyInjection.cs
@@ -0,0 +1,10 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ChronoQuest.Core.Application;
+
+public static class ApplicationDependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services) => services
+        .AddScoped<IUserActionTracker<ChapterTrackingInformation>, ChapterReadingTracker>()
+        .AddSingleton(typeof(ITrackingStore<,>), typeof(InMemoryTrackingStore<,>));
+}
