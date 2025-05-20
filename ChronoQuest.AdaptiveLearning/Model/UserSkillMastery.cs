6d7c541f309f3de5adb20c3a9d558d328e1524a8
Konstantinos Sklavenitis <konstantinos.sklaveniths@gmail.com>
2025-05-20 12:32:14 +0300

    Added summary for UserSkillMastery.cs property

--- ChronoQuest.AdaptiveLearning/Model/UserSkillMastery.cs
+++ ChronoQuest.AdaptiveLearning/Model/UserSkillMastery.cs
@@ -6,10 +6,13 @@ namespace ChronoQuest.AdaptiveLearning.Model;
 /// A user's mastery of a specific skill in a point in time.
 /// </summary>
 internal sealed class UserSkillMastery(
-    Guid modelId, 
+    Guid modelId,
     DateTimeOffset dateTime, 
     Probability probabilityOfMastery) : Entity
 {
+    /// <summary>
+    /// A reference to the <see cref="BayesianKnowledgeTracingModel"/>
+    /// </summary>
     public Guid ModelId { get; private init; } = modelId;
     public DateTime UtcDateTime { get; private init; } = dateTime.UtcDateTime;
     public Probability ProbabilityOfMastery { get; private init; } = probabilityOfMastery;
