f3f0569bd3931919664586a4fe8288781f9536b0
Konstantinos Sklavenitis <konstantinos.sklaveniths@gmail.com>
2025-05-27 12:59:16 +0300

    Change userId type

--- ChronoQuest.Core/Domain/Base/Exam.cs
+++ ChronoQuest.Core/Domain/Base/Exam.cs
@@ -3,14 +3,14 @@ namespace ChronoQuest.Core.Domain.Base;
 public class Exam : Entity
 {
     private Exam() { }
-    public Exam(Guid userId, List<Question> questions, TimeSpan timeLimit, Guid? id = null) : base(id)
+    public Exam(string userId, List<Question> questions, TimeSpan timeLimit, Guid? id = null) : base(id)
     {
         UserId = userId;
         Questions = questions;
         TimeLimit = timeLimit;
     }
 
-    public Guid UserId { get; private set; }
+    public string UserId { get; private set; } = null!;
     public List<Question> Questions { get; private set; } = null!;
     public TimeSpan TimeLimit { get; private set; }
 }
