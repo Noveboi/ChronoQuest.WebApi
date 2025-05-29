4bfc2692a7e328239b377d678f4e480ff92561c0
Nove <cptgiorgos03@gmail.com>
2025-05-30 00:02:44 +0300

    Implement GetChapterEndpoint

--- ChronoQuest.Endpoints/Chapters/GetChapterEndpoint.cs
+++ ChronoQuest.Endpoints/Chapters/GetChapterEndpoint.cs
@@ -1,11 +1,14 @@
 using ChronoQuest.Core.Application;
 using ChronoQuest.Core.Application.Chapters;
+using ChronoQuest.Core.Infrastructure;
 using ChronoQuest.Endpoints.Chapters.Dto;
 using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
 
 namespace ChronoQuest.Endpoints.Chapters;
 
-internal sealed class ReadChapterEndpoint(IUserActionTracker<ChapterTrackingInformation> tracker) : Endpoint<GetChapterRequest>
+internal sealed class GetChapterEndpoint(IUserActionTracker<ChapterTrackingInformation> tracker, ChronoQuestContext context) 
+    : Endpoint<GetChapterRequest, ChapterDto>
 {
     public override void Configure()
     {
@@ -15,7 +18,16 @@ internal sealed class ReadChapterEndpoint(IUserActionTracker<ChapterTrackingInfo
 
     public override async Task HandleAsync(GetChapterRequest req, CancellationToken ct)
     {
+        var chapter = await context.Chapters.AsNoTracking()
+            .FirstOrDefaultAsync(x => x.Id == req.ChapterId, cancellationToken: ct);
+
+        if (chapter is null)
+        {
+            await SendNotFoundAsync(ct);
+            return;
+        }
+        
         await tracker.StartTrackingAsync(userId: req.UserId, entityId: req.ChapterId, ct);
-        await SendOkAsync(ct);
+        await SendAsync(chapter.ToDto(), cancellation: ct);
     }
 }
