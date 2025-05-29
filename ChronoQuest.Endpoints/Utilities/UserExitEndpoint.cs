981fcb24642a7c25a304f7e5ab763dfa6dee6215
Nove <cptgiorgos03@gmail.com>
2025-05-30 00:02:52 +0300

    Define UserExitEndpoint

--- ChronoQuest.Endpoints/Utilities/UserExitEndpoint.cs
+++ ChronoQuest.Endpoints/Utilities/UserExitEndpoint.cs
@@ -1,6 +1,23 @@
+using System.Security.Claims;
+using FastEndpoints;
+
 namespace ChronoQuest.Endpoints.Utilities;
 
-public class UserExitEndpoint
+
+internal sealed record UserExitRequest([property: FromClaim(ClaimTypes.NameIdentifier)] Guid UserId);
+
+/// <summary>
+/// Called when the is exiting the application. 
+/// </summary>
+internal sealed class UserExitEndpoint : Endpoint<UserExitRequest>
 {
-    
+    public override void Configure()
+    {
+        Get("exit");
+    }
+
+    public override Task HandleAsync(UserExitRequest req, CancellationToken ct)
+    {
+        throw new NotImplementedException();
+    }
 }
