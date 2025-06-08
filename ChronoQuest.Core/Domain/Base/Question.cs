ccf9a4229c768dfcfae5b30af80575e551a2ba89
Konstantinos Sklavenitis <konstantinos.sklaveniths@gmail.com>
2025-06-08 20:58:58 +0300

    Add CorrectOptioId attribute in Question.cs

--- ChronoQuest.Core/Domain/Base/Question.cs
+++ ChronoQuest.Core/Domain/Base/Question.cs
@@ -3,16 +3,18 @@ namespace ChronoQuest.Core.Domain.Base;
 public class Question : Entity
 {
     private Question() { }
-    public Question(Topic topic, Difficulty difficulty, string content, List<Option> options, Guid? id = null) : base(id)
+    public Question(Topic topic, Difficulty difficulty, string content, List<Option> options, Guid correctOptionId, Guid? id = null) : base(id)
     {
         Topic = topic;
         Difficulty = difficulty;
         Content = content;
         Options = options;
+        CorrectOptionId = correctOptionId;
     }
 
     public Topic Topic { get; private set; } = null!;
-    public Difficulty Difficulty { get; private set; } 
+    public Difficulty Difficulty { get; private set; }
     public string Content { get; private set; } = null!;
     public List<Option> Options { get; private set; } = null!;
+    public Guid CorrectOptionId { get; private set; }
 }
