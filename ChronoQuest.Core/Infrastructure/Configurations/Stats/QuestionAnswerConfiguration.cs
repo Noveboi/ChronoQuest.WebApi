5c4ef77d3bc21079b07ed26abf433ebf450d6fe9
Konstantinos Sklavenitis <konstantinos.sklaveniths@gmail.com>
2025-06-08 20:58:28 +0300

    Add QuestionAnswerConfiguration.cs

---
+++ ChronoQuest.Core/Infrastructure/Configurations/Stats/QuestionAnswerConfiguration.cs
@@ -0,0 +1,19 @@
+using ChronoQuest.Core.Domain.Base;
+using ChronoQuest.Core.Domain.Stats;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ChronoQuest.Core.Infrastructure.Configurations.Stats;
+
+public class QuestionAnswerConfiguration : IEntityTypeConfiguration<QuestionAnswer>
+{
+    
+    public void Configure(EntityTypeBuilder<QuestionAnswer> builder)
+    {
+        builder.IsDomainEntity();
+        
+        builder.HasOne<User>().WithMany().HasForeignKey(x => x.UserId);
+        builder.HasOne<Question>().WithMany().HasForeignKey(x => x.QuestionId);
+        builder.HasOne<Option>().WithMany().HasForeignKey(x => x.OptionId);
+    }
+}
