240bb4f0e6f89a751491167ff2e387bececbe2bc
George Nikolaidis <nove@novesoftware.net>
2025-06-08 21:51:06 +0300

    Rename request/response objects for endpoint

--- ChronoQuest.Endpoints/Quiz/AnswerQuizQuestionEndpoint.cs
+++ ChronoQuest.Endpoints/Quiz/AnswerQuizQuestionEndpoint.cs
@@ -4,31 +4,31 @@ using Microsoft.Extensions.Logging;
 
 namespace ChronoQuest.Endpoints.Quiz;
 
-internal sealed record AnswerQuizRequest(
+internal sealed record AnswerQuizQuestionRequest(
     [property: FromClaim(ClaimTypes.NameIdentifier)] Guid UserId,
     [property: RouteParam] Guid ChapterId,
     [property: RouteParam] Guid QuestionId,
     [property: RouteParam] Guid ChosenOptionId
 );
 
-internal sealed record AnswerQuizResponse(
+internal sealed record AnswerQuizQuestionResponse(
     bool IsCorrect
 );
 
-internal sealed class AnswerQuizQuestionEndpoint : Endpoint<AnswerQuizRequest, AnswerQuizResponse>
+internal sealed class AnswerQuizQuestionEndpoint : Endpoint<AnswerQuizQuestionRequest, AnswerQuizQuestionResponse>
 {
     public override void Configure()
     {
         Post("answer/{chosenOptionId:guid}");
-        Description(x => x.Accepts<AnswerQuizRequest>());
+        Description(x => x.Accepts<AnswerQuizQuestionRequest>());
         Group<QuestionGroup>();
     }
 
-    public override async Task HandleAsync(AnswerQuizRequest req, CancellationToken ct)
+    public override async Task HandleAsync(AnswerQuizQuestionRequest req, CancellationToken ct)
     {
         // IMPORTANT: Use IQuestionService!;
         
         Logger.LogInformation("User {userId} answered question {questionId} with {optionId}", req.UserId, req.QuestionId, req.ChosenOptionId);
-        await SendAsync(new AnswerQuizResponse(true), cancellation: ct);
+        await SendAsync(new AnswerQuizQuestionResponse(true), cancellation: ct);
     }
 }
