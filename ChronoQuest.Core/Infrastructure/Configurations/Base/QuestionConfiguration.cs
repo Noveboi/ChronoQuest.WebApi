4cd437a1f59693d1d09d4b6ed979e76825143243
Konstantinos Sklavenitis <konstantinos.sklaveniths@gmail.com>
2025-06-08 20:58:00 +0300

    Refactored structure (nothing changed)

--- ChronoQuest.Core/Infrastructure/Configurations/Base/QuestionConfiguration.cs
+++ ChronoQuest.Core/Infrastructure/Configurations/Base/QuestionConfiguration.cs
@@ -8,12 +8,13 @@ public class QuestionConfiguration : IEntityTypeConfiguration<Question>
 {
     public void Configure(EntityTypeBuilder<Question> builder)
     {
+        builder.IsDomainEntity();
+        
         builder.HasOne(q => q.Topic).WithMany();
         builder.Property(q => q.Content).HasMaxLength(200).IsRequired();
         builder.OwnsMany(q => q.Options, navigationBuilder =>
         {
             navigationBuilder.Property(q => q.Content).HasMaxLength(100).IsRequired();
         });
-        builder.IsDomainEntity();
     }
 }
