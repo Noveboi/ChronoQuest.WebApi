1d159913e4ac9756346c937e1cb3c10fc31d3787
George Nikolaidis <nove@novesoftware.net>
2025-06-09 21:09:03 +0300

    Extend QuestionDto to return necessary data and update corresponding ToDto() method

--- ChronoQuest.Endpoints/Quiz/Dto/QuestionDto.cs
+++ ChronoQuest.Endpoints/Quiz/Dto/QuestionDto.cs
@@ -2,4 +2,11 @@ using ChronoQuest.Core.Domain.Base;
 
 namespace ChronoQuest.Endpoints.Quiz.Dto;
 
-internal sealed record QuestionDto(string Content, List<Option> Options);
+internal sealed record QuestionDto(
+    Guid Id, 
+    int Number, 
+    string Content,
+    string Topic,
+    string Status,
+    string Type,
+    List<Option> Options);
--- ChronoQuest.Endpoints/Quiz/Dto/QuizDtoExtensions.cs
+++ ChronoQuest.Endpoints/Quiz/Dto/QuizDtoExtensions.cs
@@ -1,12 +1,20 @@
+using ChronoQuest.Core.Application.Questions;
 using ChronoQuest.Core.Domain.Base;
 
 namespace ChronoQuest.Endpoints.Quiz.Dto;
 
 internal static class QuizDtoExtensions
 {
-    public static QuestionDto ToDto(this Question question)
+    public static QuestionDto ToDto(this QuestionResponse resp)
     {
+        var question = resp.Question;
+        
         return new QuestionDto(
+            Id: question.Id,
+            Number: question.Number,
+            Topic: question.Topic.Name,
+            Type: question.Type.ToString(),
+            Status: resp.Status.ToString(),
             Content: question.Content,
             Options: question.Options);
     }
