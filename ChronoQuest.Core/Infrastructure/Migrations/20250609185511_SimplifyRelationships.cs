6ff56c8bcd9f5a4a7dbbc595d5f70c6a38fd3ab1
George Nikolaidis <nove@novesoftware.net>
2025-06-09 22:03:32 +0300

    Add migration

---
+++ ChronoQuest.Core/Infrastructure/Migrations/20250609185511_SimplifyRelationships.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChronoQuest.Core.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SimplifyRelationships : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Chapters_Quiz_QuizId",
+                table: "Chapters");
+
+            migrationBuilder.DropTable(
+                name: "QuestionQuiz");
+
+            migrationBuilder.DropTable(
+                name: "Quiz");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Chapters_QuizId",
+                table: "Chapters");
+
+            migrationBuilder.DropColumn(
+                name: "QuizId",
+                table: "Chapters");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ChapterId",
+                table: "Questions",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Number",
+                table: "Questions",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Type",
+                table: "Questions",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Questions_ChapterId",
+                table: "Questions",
+                column: "ChapterId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Questions_Chapters_ChapterId",
+                table: "Questions",
+                column: "ChapterId",
+                principalTable: "Chapters",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Questions_Chapters_ChapterId",
+                table: "Questions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Questions_ChapterId",
+                table: "Questions");
+
+            migrationBuilder.DropColumn(
+                name: "ChapterId",
+                table: "Questions");
+
+            migrationBuilder.DropColumn(
+                name: "Number",
+                table: "Questions");
+
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Questions");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "QuizId",
+                table: "Chapters",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateTable(
+                name: "Quiz",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Quiz", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "QuestionQuiz",
+                columns: table => new
+                {
+                    QuestionsId = table.Column<Guid>(type: "uuid", nullable: false),
+                    QuizId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QuestionQuiz", x => new { x.QuestionsId, x.QuizId });
+                    table.ForeignKey(
+                        name: "FK_QuestionQuiz_Questions_QuestionsId",
+                        column: x => x.QuestionsId,
+                        principalTable: "Questions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_QuestionQuiz_Quiz_QuizId",
+                        column: x => x.QuizId,
+                        principalTable: "Quiz",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Chapters_QuizId",
+                table: "Chapters",
+                column: "QuizId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QuestionQuiz_QuizId",
+                table: "QuestionQuiz",
+                column: "QuizId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Chapters_Quiz_QuizId",
+                table: "Chapters",
+                column: "QuizId",
+                principalTable: "Quiz",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
