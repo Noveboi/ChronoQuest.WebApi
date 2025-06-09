76db1882c904d8af79f896647a44db406a501886
George Nikolaidis <nove@novesoftware.net>
2025-06-10 01:04:08 +0300

    Add Async suffix to GetQuestionForChapter

--- ChronoQuest.Core/Application/Questions/IQuestionService.cs
+++ ChronoQuest.Core/Application/Questions/IQuestionService.cs
@@ -5,7 +5,7 @@ namespace ChronoQuest.Core.Application.Questions;
 
 public interface IQuestionService
 {
-    Task<List<QuestionResponse>> GetQuestionsForChapter(QuestionsForChapterRequest request, CancellationToken token);
+    Task<List<QuestionResponse>> GetQuestionsForChapterAsync(QuestionsForChapterRequest request, CancellationToken token);
     Task<QuestionResponse?> GetQuestionAsync(QuestionRequest request, CancellationToken token);
     Task<Result<QuestionAnswer>> AnswerQuestionAsync(AnswerQuestionRequest request, CancellationToken token);
 }
