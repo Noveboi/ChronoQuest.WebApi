9d73e8b38ebffe928f7452e7a9fd9064abc9a583
George Nikolaidis <nove@novesoftware.net>
2025-06-09 21:10:34 +0300

    Add QuestionResponse

---
+++ ChronoQuest.Core/Application/Questions/QuestionResponse.cs
@@ -0,0 +1,21 @@
+﻿using ChronoQuest.Core.Domain.Base;
+
+namespace ChronoQuest.Core.Application.Questions;
+
+public sealed class QuestionResponse
+{
+    internal QuestionResponse(Question question)
+    {
+        Question = question;
+
+        Status = question.Answers switch
+        {
+            [] => QuestionStatus.Unanswered,
+            [.., { IsCorrect: true }] => QuestionStatus.Correct,
+            [.., { IsCorrect: false }] => QuestionStatus.Wrong
+        };
+    }
+    
+    public Question Question { get; }
+    public QuestionStatus Status { get; }
+}
