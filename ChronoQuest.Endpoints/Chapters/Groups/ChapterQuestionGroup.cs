bb334db557d6cd1f4d20a24048aa04ceb847639a
George Nikolaidis <nove@novesoftware.net>
2025-06-10 01:06:43 +0300

    Refactor ChapterQuestionGroup

--- ChronoQuest.Endpoints/Chapters/Groups/ChapterQuestionGroup.cs
+++ ChronoQuest.Endpoints/Chapters/Groups/ChapterQuestionGroup.cs
@@ -4,6 +4,5 @@ namespace ChronoQuest.Endpoints.Chapters.Groups;
 
 internal sealed class ChapterQuestionGroup : SubGroup<ChapterGroup>
 {
-    // We don't need to specify a Quiz ID here because there is one quiz per chapter.
-    public ChapterQuestionGroup() => Configure("{chapterId:guid}/quiz", _ => { });
+    public ChapterQuestionGroup() => Configure("{chapterId:guid}/questions", _ => { });
 }
