d7960e6deee687fb1af71f5901a53db4f411f944
George Nikolaidis <nove@novesoftware.net>
2025-06-10 16:05:04 +0300

    Add --force flag to drop-database script

--- drop-database.sh
+++ drop-database.sh
@@ -1,3 +1,3 @@
 docker compose up -d --build || exit 1
 dotnet tool install dotnet-ef --create-manifest-if-needed
-dotnet ef database drop --project ./ChronoQuest.Core --startup-project ./ChronoQuest.WebApi
+dotnet ef database drop --project ./ChronoQuest.Core --startup-project ./ChronoQuest.WebApi --force
