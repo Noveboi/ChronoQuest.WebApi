dc2d9b32c19b186490b77fd7235b0d80fb94c680
George Nikolaidis <nove@novesoftware.net>
2025-06-11 16:57:17 +0300

    Simplify GetMasteryHistory endpoint request

--- ChronoQuest.Endpoints/AdaptiveLearning/GetMasteryHistoryEndpoint.cs
+++ ChronoQuest.Endpoints/AdaptiveLearning/GetMasteryHistoryEndpoint.cs
@@ -2,23 +2,21 @@
 using ChronoQuest.Core.Application.Adaptive;
 using ChronoQuest.Endpoints.AdaptiveLearning.Dto;
 using ChronoQuest.Endpoints.Questions.Dto;
+using ChronoQuest.Endpoints.Utilities;
+using ChronoQuest.Endpoints.Utilities.Attributes;
 using FastEndpoints;
 
 namespace ChronoQuest.Endpoints.AdaptiveLearning;
 
-internal sealed record GetMasteryHistoryRequest(
-    [property: FromClaim(ClaimTypes.NameIdentifier)] Guid UserId,
-    [property: RouteParam] Guid TopicId);
-
 internal sealed class GetMasteryHistoryEndpoint(IAdaptiveLearning adaptiveLearning) 
-    : Endpoint<GetMasteryHistoryRequest, IEnumerable<UserMasteryHistoryDto>>
+    : Endpoint<GetRequest, IEnumerable<UserMasteryHistoryDto>>
 {
     public override void Configure()
     {
         Get("/mastery");
     }
 
-    public override async Task HandleAsync(GetMasteryHistoryRequest req, CancellationToken ct)
+    public override async Task HandleAsync(GetRequest req, CancellationToken ct)
     {
         var history = await adaptiveLearning.GetMasteryOverTimeAsync(req.UserId, ct);
         
