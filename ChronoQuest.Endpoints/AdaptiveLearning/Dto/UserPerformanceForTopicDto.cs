f43e07d7f0aa07ca0c1e633ced9cc247231e72cd
George Nikolaidis <nove@novesoftware.net>
2025-06-11 17:00:45 +0300

    Add GET /performance endpoint

---
+++ ChronoQuest.Endpoints/AdaptiveLearning/Dto/UserPerformanceForTopicDto.cs
@@ -0,0 +1,8 @@
+﻿using ChronoQuest.Endpoints.Questions.Dto;
+
+namespace ChronoQuest.Endpoints.AdaptiveLearning.Dto;
+
+internal sealed record UserPerformanceForTopicDto(
+    double Score,
+    string State,
+    TopicDto Topic);
---
+++ ChronoQuest.Endpoints/AdaptiveLearning/GetUserPerformanceEndpoint.cs
@@ -0,0 +1,26 @@
+﻿using ChronoQuest.Core.Application.Adaptive;
+using ChronoQuest.Endpoints.AdaptiveLearning.Dto;
+using ChronoQuest.Endpoints.Questions.Dto;
+using ChronoQuest.Endpoints.Utilities;
+using FastEndpoints;
+
+namespace ChronoQuest.Endpoints.AdaptiveLearning;
+
+internal sealed class GetUserPerformanceEndpoint(IAdaptiveLearning adaptiveLearning) 
+    : Endpoint<GetRequest, IEnumerable<UserPerformanceForTopicDto>>
+{
+    public override void Configure()
+    {
+        Get("/performance");
+    }
+
+    public override async Task HandleAsync(GetRequest req, CancellationToken ct)
+    {
+        var performances = await adaptiveLearning.GetPerformanceAsync(req.UserId, ct);
+        
+        await SendAsync(performances.Select(x => new UserPerformanceForTopicDto(
+            Score: x.Performance.TotalScore,
+            State: x.Performance.LearningProgress.State.ToString(),
+            Topic: x.Topic.ToDto())), cancellation: ct);
+    }
+}
