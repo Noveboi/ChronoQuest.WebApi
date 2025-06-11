3ec263b9349a96bd3b6b6d5244dc44c7126149d4
George Nikolaidis <nove@novesoftware.net>
2025-06-11 11:27:49 +0300

    Document GetExamEndpoint

--- ChronoQuest.Endpoints/Exam/GetExamEndpoint.cs
+++ ChronoQuest.Endpoints/Exam/GetExamEndpoint.cs
@@ -2,5 +2,8 @@ namespace ChronoQuest.Endpoints.Exam;
 
 public class GetExamEndpoint
 {
+    // Όπως στο extra material, 2 cases...
     
+    // Case 1: Exam for user not found
+    // Case 2: Exam for user found
 }
