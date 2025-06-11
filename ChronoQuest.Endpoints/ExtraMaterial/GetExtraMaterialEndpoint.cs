ffc8205b756e48b321bf569adf6d564576a351c3
Konstantinos Sklavenitis <konstantinos.sklaveniths@gmail.com>
2025-06-11 19:49:32 +0300

    Cleanup commit

--- ChronoQuest.Endpoints/ExtraMaterial/GetExtraMaterialEndpoint.cs
+++ ChronoQuest.Endpoints/ExtraMaterial/GetExtraMaterialEndpoint.cs
@@ -9,11 +9,6 @@ namespace ChronoQuest.Endpoints.ExtraMaterial;
 
 internal sealed class GetExtraMaterialEndpoint(ChronoQuestContext dbContext, ExtraMaterialGenerator generator) : Endpoint<GetRequest, ExtraMaterialDto>
 {
-    // Two cases:
-    // - Case 1: Extra material for user exists! Simply return the material as a DTO
-    // - Case 2: Extra material for user does not exist! First generate the material, add it to the DB and then return 
-    //           it as a DTO.
-
     public override void Configure()
     {
         Get("extra-material");
