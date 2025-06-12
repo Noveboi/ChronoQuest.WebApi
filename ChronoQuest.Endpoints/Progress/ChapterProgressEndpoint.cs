7f91d6ed13b7083967011d87481a15cea4fadbb3
George Nikolaidis <nove@novesoftware.net>
2025-06-12 17:17:59 +0300

    Add chapter progress endpoint

---
+++ ChronoQuest.Endpoints/Progress/ChapterProgressEndpoint.cs
@@ -0,0 +1,25 @@
+﻿using ChronoQuest.Core.Application.Progress;
+using ChronoQuest.Endpoints.Utilities;
+using FastEndpoints;
+
+namespace ChronoQuest.Endpoints.Progress;
+
+internal sealed record ChapterProgressDto(string Status);
+
+internal sealed class ChapterProgressEndpoint(IProgressQueries progress) : Endpoint<GetRequest, ChapterProgressDto>  
+{
+    public override void Configure()
+    {
+        Get("chapters");
+        Group<ProgressGroup>();
+    }
+
+    public override async Task HandleAsync(GetRequest req, CancellationToken ct)
+    {
+        var result = await progress.HasCompletedAllChapters(req.UserId, ct);
+
+        await SendAsync(new ChapterProgressDto(result.IsSuccess
+            ? "completed"
+            : "ongoing"), cancellation: ct);
+    }
+}
---
+++ ChronoQuest.Endpoints/Progress/ProgressGroup.cs
@@ -0,0 +1,11 @@
+﻿using FastEndpoints;
+
+namespace ChronoQuest.Endpoints.Progress;
+
+internal sealed class ProgressGroup : Group
+{
+    public ProgressGroup()
+    {
+        Configure("progress", _ => {});
+    }
+}
