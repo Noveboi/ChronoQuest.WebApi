d1f7d433f4f7d3ac7f869dd7e33d034511fb6c80
George Nikolaidis <nove@novesoftware.net>
2025-06-12 15:22:22 +0300

    Implement ReviewMaterialGenerator

--- ChronoQuest.Core/Application/Review/ReviewMaterialGenerator.cs
+++ ChronoQuest.Core/Application/Review/ReviewMaterialGenerator.cs
@@ -1,4 +1,6 @@
-﻿using ChronoQuest.Core.Application.Adaptive;
+﻿using System.Text;
+using ChronoQuest.Core.Application.Adaptive;
+using ChronoQuest.Core.Domain.AdaptiveLearning.Metrics;
 using ChronoQuest.Core.Domain.Base;
 using ChronoQuest.Core.Infrastructure;
 using Microsoft.EntityFrameworkCore;
@@ -7,10 +9,55 @@ namespace ChronoQuest.Core.Application.Review;
 
 public sealed class ReviewMaterialGenerator(IAdaptiveLearning adaptiveLearning, ChronoQuestContext context)
 {
+    private const int MaxParagraphs = 6;
+    
     public async Task<ReviewMaterial> GenerateAsync(Guid userId, CancellationToken token)
     {
         var performances = await adaptiveLearning.GetPerformanceAsync(userId, token);
-        var reviewParagraphs = await context.ReviewParagraphs.ToListAsync(token);
-        throw new NotImplementedException();
+        var reviewParagraphs = await context.ReviewParagraphs
+            .OrderBy(x => x.Order)
+            .GroupBy(x => x.Topic)
+            .ToDictionaryAsync(
+                keySelector: x => x.Key,
+                elementSelector: x => x.ToList(), 
+                cancellationToken: token);
+
+        var paragraphs = performances
+            .SelectMany(performance =>
+            {
+                var paragraphsForTopic = reviewParagraphs[performance.Topic];
+                var numberOfParagraphs = DetermineNumberOfParagraphs(performance);
+
+                return paragraphsForTopic.Take(numberOfParagraphs);
+            })
+            .Aggregate(new StringBuilder(), (builder, paragraph) => builder.AppendLine(paragraph.Content))
+            .ToString();
+
+        return new ReviewMaterial(userId, paragraphs);
+    }
+
+    private static int DetermineNumberOfParagraphs(UserPerformanceForTopic performance)
+    {
+        var learning = performance.Performance.LearningProgress;
+
+        var confidenceScalar = learning.Confidence switch
+        {
+            <= 0.7 => 1,
+            _ => 0
+        };
+
+        var statusScalar = learning.State switch
+        {
+            LearningState.Initial => MaxParagraphs,
+            LearningState.Struggling => MaxParagraphs,
+            LearningState.StrugglingButImproving => MaxParagraphs - 1,
+            LearningState.ActiveLearning => 3,
+            LearningState.Steady => 2,
+            LearningState.Plateau => 3,
+            LearningState.Mastering => 1,
+            _ => 0
+        };
+
+        return Math.Min(MaxParagraphs, Math.Max(0, confidenceScalar + statusScalar));
     }
 }
