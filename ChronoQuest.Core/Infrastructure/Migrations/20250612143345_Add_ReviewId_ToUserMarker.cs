04b29ccecedf5b204fefcc6189afd450bf456656
George Nikolaidis <nove@novesoftware.net>
2025-06-12 17:33:55 +0300

    Add migration for new UserMarker property

---
+++ ChronoQuest.Core/Infrastructure/Migrations/20250612143345_Add_ReviewId_ToUserMarker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChronoQuest.Core.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_ReviewId_ToUserMarker : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ReviewId",
+                table: "Markers",
+                type: "uuid",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReviewId",
+                table: "Markers");
+        }
+    }
+}
