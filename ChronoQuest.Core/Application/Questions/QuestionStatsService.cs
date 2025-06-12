ffcca1d0ba38086dc3d4e77c52b93051d6f59223
Konstantinos Sklavenitis <konstantinos.sklaveniths@gmail.com>
2025-06-12 12:32:37 +0300

    Implement QuestionStatsService.cs

--- ChronoQuest.Core/Application/Questions/QuestionStatsService.cs
+++ ChronoQuest.Core/Application/Questions/QuestionStatsService.cs
@@ -1,9 +1,30 @@
-﻿namespace ChronoQuest.Core.Application.Questions;
+﻿using ChronoQuest.Core.Infrastructure;
+using Microsoft.EntityFrameworkCore;
 
-public class QuestionStatsService
+namespace ChronoQuest.Core.Application.Questions;
+
+public class QuestionStatsService(ChronoQuestContext dbContext)
 {
-    public Task<QuestionStats> GetAsync(Guid userId, CancellationToken token)
+    public async Task<IEnumerable<QuestionStatsForTopic>> GetAsync(Guid userId, CancellationToken ct)
     {
-        throw new NotImplementedException();
+        var questions = await dbContext
+            .Questions
+            .WithReadingTimeOf(userId)
+            .WithAnswersOf(userId)
+            .GroupBy(q => q.Topic)
+            .ToListAsync(ct);
+
+        var stats = questions
+            .Select(group => new QuestionStatsForTopic(
+                Topic: group.Key,
+                CorrectAnswersPercentage: group.SelectMany(q => q.Answers).Count(qa => qa.IsCorrect) /
+                (double)group.SelectMany(q => q.Answers).Count() * 100,
+                AverageAnswerTime: TimeSpan.FromSeconds(group
+                    .SelectMany(q => q.ReadingTime
+                        .Select(qrt => qrt.Duration.TotalSeconds))
+                    .Average())
+            ));
+        
+        return stats;
     }
 }
