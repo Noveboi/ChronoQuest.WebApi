0ca805754b6e258218045573d887af681726e117
George Nikolaidis <nove@novesoftware.net>
2025-06-12 14:43:50 +0300

    Move ExamTimer and rename

---
+++ ChronoQuest.Core/Domain/Stats/ExamTimeInformation.cs
@@ -0,0 +1,12 @@
+﻿using ChronoQuest.Core.Application.Tracking;
+using ChronoQuest.Core.Domain.Interfaces;
+
+namespace ChronoQuest.Core.Domain.Stats;
+
+public sealed record ExamTimeInformation(TimeSpan ElapsedTime) : ITimeTrackingEntity<ExamTimeInformation>
+{
+    public static ExamTimeInformation FromData(TimeTrackingInformation info, Guid userId)
+    {
+        return new ExamTimeInformation(info.Duration);
+    }
+}
