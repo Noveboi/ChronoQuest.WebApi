01be130d3ca6fcde2fc75f1b313712ee1392e185
George Nikolaidis <nove@novesoftware.net>
2025-06-12 18:28:06 +0300

    Update UserMarkerDto

--- ChronoQuest.Endpoints/Utilities/Dto/UserMarkerDto.cs
+++ ChronoQuest.Endpoints/Utilities/Dto/UserMarkerDto.cs
@@ -2,4 +2,4 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace ChronoQuest.Endpoints.Utilities.Dto;
 
-internal sealed record UserMarkerDto(Guid? ChapterId, Guid? QuestionId, Guid? ExamId);
+internal sealed record UserMarkerDto(Guid? ChapterId, Guid? QuestionId, Guid? ExamId, Guid? ReviewId);
--- ChronoQuest.Endpoints/Utilities/Dto/UtilityDtoExtensions.cs
+++ ChronoQuest.Endpoints/Utilities/Dto/UtilityDtoExtensions.cs
@@ -6,9 +6,10 @@ internal static class UtilityDtoExtensions
 {
     public static UserMarkerDto ToDto(this UserMarker? marker) =>
         marker is null
-            ? new UserMarkerDto(null, null, null)
+            ? new UserMarkerDto(null, null, null, null)
             : new UserMarkerDto(
                 ChapterId: marker.ChapterId, 
                 QuestionId: marker.QuestionId, 
-                ExamId: marker.ExamId);
+                ExamId: marker.ExamId,
+                ReviewId: marker.ReviewId);
 }
