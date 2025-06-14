c8c87cd0f4dd5c667d34960f09d343cdd24c5d5f
George Nikolaidis <nove@novesoftware.net>
2025-06-14 09:43:27 +0300

    Add migration

---
+++ ChronoQuest.Core/Infrastructure/Migrations/20250614064311_RemoveQuestionNumber.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChronoQuest.Core.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveQuestionNumber : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Number",
+                table: "Questions");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Number",
+                table: "Questions",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
--- ChronoQuest.Endpoints/Exams/ExamTestEndpoint.cs
+++ ChronoQuest.Endpoints/Exams/ExamTestEndpoint.cs
@@ -20,7 +20,7 @@ internal sealed class ExamTestEndpoint(ExamGenerator generator) : Endpoint<GetRe
             response: new ExamDto(
                 Id: exam.Id,
                 TimeLimitInSeconds: exam.TimeLimit.TotalSeconds,
-                Questions: exam.Questions.Select(x => x.ToPreviewDto()).OrderBy(x => x.Number)), 
+                Questions: exam.Questions.Select(x => x.ToPreviewDto())), 
             cancellation: ct);
     }
 }
